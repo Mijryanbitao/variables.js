35c567b6b61251120c3fb9779c5e5bc5c4a25fd4
Mijryanbitao <mijryanb@gmail.com>
2024-09-10 09:13:17 +0800

    Create app.js

---
+++ app.js
@@ -0,0 +1,19 @@
+// create a variable named name and store you name
+let name = ' Bitao, Mij Ryan ';
+
+// create a variable named whatDoYouWannaBecomeInYourLife and store "web developer"
+let whatDoYouWannaBecomeInYourLife = 'Web Developer';
+
+// create a variable named gender and store your gender
+let gender = 'Male';
+
+// create a variable named InstagramHandle and store you InstagramHandle
+let InstagramHandle = '_mjryn';
+
+// log all the variable to console
+console.log('What Is your name? ', name);
+consone.log('What do you wanna become in your life? ', whatDoYouWannaBecomeInYourLife);
+console.log('Are you a male or female? ', gender);
+console.log('My Instagram: ', InstagramHandler);
+
+
